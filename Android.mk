ac8b52f08250f6363915c0d1cad461be3f827787
Kshitij Gupta <kshitijgm@gmail.com>
2020-04-07 18:01:10 +0000

    v7101o: Create symlinks for gatekeeper

--- Android.mk
+++ Android.mk
@@ -7,5 +7,19 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),v7101o)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+GATEKEEPER_SYMLINKS := $(TARGET_OUT_VENDOR)
+$(GATEKEEPER_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating gatekeeper symlinks: $@"
+	@mkdir -p $@/lib/hw
+	@mkdir -p $@/lib64/hw
+	@ln -sf $@/lib/hw/libSoftGatekeeper.so $@/lib/hw/gatekeeper.default.so
+	@ln -sf $@/lib64/hw/libSoftGatekeeper.so $@/lib64/hw/gatekeeper.default.so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINKS)
+
 endif
