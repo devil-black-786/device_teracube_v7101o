37c6b7bbc9b529be0ba66252dbc9750cfd1abe8a
TheMalachite <eliasgheeraert@gmail.com>
2020-12-27 20:52:47 +0100

    v7101o: Switch to audio@6.0 HALs and HIDL from atom

--- device.mk
+++ device.mk
@@ -16,7 +16,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     tinymix
