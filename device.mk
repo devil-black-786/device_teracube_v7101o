161afec04977d65d4a75041a8558ad88880058e0
TheMalachite <eliasgheeraert@gmail.com>
2020-12-29 00:20:37 +0100

    v7101o: Drop prebuilt soundtrigger, audio.common blobs

--- device.mk
+++ device.mk
@@ -16,6 +16,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio.common-util \
+    android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
